770a423756d4be03fe399de26edb3606c354aeef
Psykepro <nikolai_nedkov@yahoo.com>
2017-04-26 05:27:42 +0300

    Test

--- WYF.ClientApp/WYF.ClientApp/App/Business/User/UserService.js
+++ WYF.ClientApp/WYF.ClientApp/App/Business/User/UserService.js
@@ -8,25 +8,11 @@
     function UserService() {
 
         var instance = {
-            Login: Login
+           
         };
 
         return instance;
 
-        function Login(username, password, onSuccessCallBack) {
-
-            var dto = {
-                username: username,
-                password: password
-            };
-
-            var url = URL_WITH_PORT + "/api/Token";
-            $http.post(url, dto).then(function (result) {
-                if (onSuccessCallBack != null && typeof onSuccessCallBack == 'function') onSuccessCallBack(result.data);
-            }, function erro(result) {
-                var a = 5;
-            });
-        }
     }
 
 })();
