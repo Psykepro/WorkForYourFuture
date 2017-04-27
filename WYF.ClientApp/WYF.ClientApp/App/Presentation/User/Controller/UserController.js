4868263d963cab4cb3c0affe29065a361be51a63
Psykepro <nikolai_nedkov@yahoo.com>
2017-04-27 21:01:43 +0300

    Adding one more notifier
    Added one more notifier and now UserController is setting the access
    token and the username to the localStorage on successful login

--- WYF.ClientApp/WYF.ClientApp/App/Presentation/User/Controller/UserController.js
+++ WYF.ClientApp/WYF.ClientApp/App/Presentation/User/Controller/UserController.js
@@ -18,21 +18,24 @@
                 userService
                     .Login("ps@abv.bg", "Aa!1234")
                     .then(function success(result) {
-                        iziToast.success({
-                            timeout: 2000,
-                            imageWidth: 150,
-                            position: "topCenter",
-                            title: 'OK',
-                            message: 'The login was successful!',
+                        var accessToken = result.access_token;
+                        var userName = result.userName;
+                        localStorage.setItem("accessToken", accessToken);
+                        localStorage.setItem("userName", userName);
+                        notie.alert({
+                            type: 'success',
+                            text: "The login was successful!",
+                            position: 'bottom'
                         });
-                    },function failure(error) {
-                        iziToast.error({
-                            title: 'ERR',
-                            message: "The login wasn't successful!",
+                    }, function failure(error) {
+                        notie.alert({
+                            type: 'error',
+                            text: "The login wasn't successful!",
+                            position: 'bottom'
                         });
-                        });
+                    });
             }
-            
+
         }
 
         return instance;
