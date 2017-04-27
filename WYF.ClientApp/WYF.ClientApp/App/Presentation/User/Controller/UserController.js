9a9ca8fa047bd5d1ed1f8f31ee08d679845be2bf
Psykepro <nikolai_nedkov@yahoo.com>
2017-04-27 20:43:12 +0300

    Addint notifier IziToast

--- WYF.ClientApp/WYF.ClientApp/App/Presentation/User/Controller/UserController.js
+++ WYF.ClientApp/WYF.ClientApp/App/Presentation/User/Controller/UserController.js
@@ -15,7 +15,22 @@
             isFormValid = $scope.loginForm.$valid;
 
             if (isFormValid) {
-                userService.Login(this.username, this.password);
+                userService
+                    .Login("ps@abv.bg", "Aa!1234")
+                    .then(function success(result) {
+                        iziToast.success({
+                            timeout: 2000,
+                            imageWidth: 150,
+                            position: "topCenter",
+                            title: 'OK',
+                            message: 'The login was successful!',
+                        });
+                    },function failure(error) {
+                        iziToast.error({
+                            title: 'ERR',
+                            message: "The login wasn't successful!",
+                        });
+                        });
             }
             
         }
