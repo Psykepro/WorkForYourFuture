c1410d5acc13dcfdaa7cb44a39db490a7bfc4772
psykepro <psykepro@abv.bg>
2017-05-01 23:14:19 +0300

    Adding Common Controller

---
+++ WYF.WebAPI/WYF.WebAPI/Controllers/CommonController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WYF.WebAPI.Data;
+
+namespace WYF.WebAPI.Controllers
+{
+    [RoutePrefix("api/Common")]
+    public class CommonController : ApiController
+    {
+        private WyfDbContext _context = WyfDbContext.Create();
+
+        //[Authorize]
+        [Route("Cities")]
+        [HttpGet]
+        public IEnumerable<string> Cities()
+        {
+            string[] allCities = _context.Cities.Select(c => c.Name).ToArray();
+
+            if (allCities == null || allCities.Length == 0)
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent("There are no Cities in the database."),
+                    ReasonPhrase = "Missing Resource Exception"
+                });
+            }
+
+            return allCities;
+        }
+    }
+}
--- WYF.WebAPI/WYF.WebAPI/Controllers/JobController.cs
+++ WYF.WebAPI/WYF.WebAPI/Controllers/JobController.cs
@@ -14,23 +14,6 @@ namespace WYF.WebAPI.Controllers
     {
         private WyfDbContext _context = WyfDbContext.Create();
 
-        //[Authorize]
-        [Route("Cities")]
-        [HttpGet]
-        public IEnumerable<string> Cities()
-        {
-            string[] allCities = _context.Cities.Select(c => c.Name).ToArray();
-            
-            if (allCities == null || allCities.Length == 0)
-            {
-                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    Content = new StringContent("There are no Cities in the database."),
-                    ReasonPhrase = "Missing Resource Exception"
-                });
-            }
-
-            return allCities;
-        }
+        
     }
 }
